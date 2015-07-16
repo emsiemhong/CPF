1a2291fd20977410215096e45673359d8f55ec6f
Siemhong <emsiemhong666@gmail.com>
2015-07-17 00:01:32 +0700

    Create contact controller view

---
+++ NGS/src/NGS/ContentBundle/Form/ContactType.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace NGS\ContentBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Collection;
+
+class ContactType extends AbstractType
+{
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator) {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add('name', 'text', array(
+                'attr' => array(
+                    'placeholder' => 'What\'s your name?',
+                    'pattern'     => '.{2,}' //minlength
+                )
+            ))
+            ->add('email', 'email', array(
+                'attr' => array(
+                    'placeholder' => 'So I can get back to you.'
+                )
+            ))
+            ->add('subject', 'text', array(
+                'attr' => array(
+                    'placeholder' => 'The subject of your message.',
+                    'pattern'     => '.{3,}' //minlength
+                )
+            ))
+            ->add('message', 'textarea', array(
+                'attr' => array(
+                    'cols' => 90,
+                    'rows' => 10,
+                    'placeholder' => 'And your message to me...'
+                )
+            ));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+         $collectionConstraint = new Collection(array(
+            'name' => array(
+                new NotBlank(array('message' => 'Name should not be blank.')),
+                new Length(array('min' => 2))
+            ),
+            'email' => array(
+                new NotBlank(array('message' => 'Email should not be blank.')),
+                new Email(array('message' => 'Invalid email address.'))
+            ),
+            'subject' => array(
+                new NotBlank(array('message' => 'Subject should not be blank.')),
+                new Length(array('min' => 3))
+            ),
+            'message' => array(
+                new NotBlank(array('message' => 'Message should not be blank.')),
+                new Length(array('min' => 5))
+            )
+        ));
+
+        $resolver->setDefaults(array(
+            'constraints' => $collectionConstraint
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ngs_contentbundle_contact';
+    }
+}
